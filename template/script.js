e491b8a97c34806789c283cdee57951a34939745
Devin Bahramand <Devin@Devins-MacBook-Pro.local>
2016-05-04 20:39:53 -0700

    worked on an upgrade section

--- template/script.js
+++ template/script.js
@@ -5,11 +5,41 @@
 	//create a function called onClick that adds 1 to the var clickCount whenever the image with the id "clicker" is clicked.
 
 var clickCount = 0
-document.getElementById("clicker").addEventListener('click', function () {
-	clickCount += 1;
+
+document.getElementById("clicker").addEventListener('click', function() {
+	if (upgradeOnePurchases >= 1) {
+		clickCount += 2
+	}
+	else {
+		clickCount += 1
+	}
 	document.getElementById("clickerCount").innerHTML = clickCount + " " + "Points"; 
 })
 
-//Step Two, 
+//Step Two, The Upgrade Shop
+	//
+var upgradeOnePurchases = 0
+
+ document.getElementById("upgradeOne").addEventListener('click', function() {
+ 	if (clickCount >= 25 && upgradeOnePurchases == 0) {
+ 		clickCount -= 25;
+ 		document.getElementById("clickerCount").innerHTML = clickCount + " " + "Points";
+ 		upgradeOnePurchases += 1;
+ 	}
+ 	else if (clickCount >= 50 && upgradeOnePurchases == 1) {
+ 		clickCount -= 50;
+ 		document.getElementById("clickerCount").innerHTML = clickCount + " " + "Points";
+ 		upgradeOnePurchases += 1;
+ 	}
+ 	else {
+ 		alert("You do not have enough points!");
+ 	}
+ })
+ document.getElementById("upgradeOne").onmouseover = function() {
+ 	document.getElementById('upgradeOneDescription').style.display = 'block';
+ }
+ document.getElementById("upgradeOne").onmouseout = function() {
+ 	document.getElementById('upgradeOneDescription').style.display = 'none';
+ }
 
 
